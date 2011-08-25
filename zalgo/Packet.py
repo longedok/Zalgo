d428d0f0d121bb2ed80ed328bd28b2dbbc72b286
Evgeniy K <longedok@gmail.com>
2011-08-25 22:14:42 +0400

    Rewrite Packet() class. Now it more high-level.

--- zalgo/Packet.py
+++ zalgo/Packet.py
@@ -1,25 +1,49 @@
-import struct
 import json
+import time
+import random
 
 from Debug import debug
 
 class Packet(object):
-    def __init__(self, type=-1, ex_headers=[], content=''):
-        self.type = type
-        self.extra_headers = ex_headers
-        self.content = content
-        self.content_len = len(content)
-        self.extra_headers_len = 0
 
-    def set_extra_headers(self, headers_list):
-        self.extra_headers = headers_list
+    def __init__(self, _type=-1, header=dict(), content=str()):
+        self.__header = dict()
+        self.__content = str()
+        self.set_header(header)
+        self.set_content(content)
+        self.set_header_field('packet_id', self.__generate_id())
+        self.set_header_field('type', _type)
+        self.set_header_field('content_len', len(self.get_content()))
+
+    def __generate_id(self):
+        unix_time = str(int(time.time() * 1000))
+        random_postfix = ''.join([chr(random.randint(ord('a'), ord('z'))) for _ in xrange(4)])
+        return unix_time + random_postfix
+
+    def set_content(self, content):
+        self.__content = content
+
+    def get_content(self):
+        return self.__content
+
+    def set_header(self, header):
+        self.__header = header
+
+    def get_header(self):
+        return self.__header
+
+    def set_header_field(self, field_name, value):
+        self.__header[field_name] = value
+
+    def get_header_field(self, field_name):
+        return self.__header.get(field_name.lower())
 
     def get_binary(self):
-        content = self.content
-        ex_heads = json.dumps(self.extra_headers)
-        header = struct.pack('!Bii', self.type, len(content), len(ex_heads))
-        return header + ex_heads + content
+        return json.dumps(self.__header) + '\0' + self.__content
 
     def __str__(self):
-        return "Content len: %d; Extra headers: %s; Content[:10]: %s" % (self.content_len, 
-                self.extra_headers, self.content[:10])
+        return "Header: %s" % (self.__header)
+
+if __name__ == '__main__':
+    p = Packet(1, {'title': 'starlight'})
+    print p.get_header()
